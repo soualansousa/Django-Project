877651ad39c985b34e0fac162c68f3ba75f8acbe
Alan dos Santos Sousa <soualansousa@gmail.com>
2024-01-11 10:29:02 -0300

    Removendo url contato

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from recipes.views import contato, home
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def contato(request):
-    return HttpResponse('Contato')
-
 def home(request):
     return render(request, 'recipes/home.html')
