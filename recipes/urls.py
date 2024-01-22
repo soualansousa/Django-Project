c2b5b9f22265b92d8c77803bee57e6dee54ed655
Alan dos Santos Sousa <soualansousa@gmail.com>
2024-01-22 18:16:47 -0300

    Algumas modificações que ficaram pendentes na seção 7

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='recipe-home'),
+    path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
 ]
